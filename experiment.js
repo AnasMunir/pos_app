9a35b95860abdbdffd56350d0619af38c142498c
AnasMunir <anas.munir.92@gmail.com>
2016-10-01 04:38:27 +0500

    Finally got the secondary indexes to work i think error was in naming convention when quering

--- experiment.js
+++ experiment.js
@@ -7,46 +7,81 @@ var $ = document.querySelector.bind(document);
 //db.plugin(require('pouchdb-upsert');
 
 var type = 'usb';
-var brand = 'kingston';
-var model = 'moto-x';
+var brand = 'sandisk';
+var model = '3.0';
 var color = '';
 var storage = '16gb';
 
+new PouchDB('experiment5-db').destroy().then(function () {
+  return new PouchDB('experiment5-db');
+}).then(function (db) {
 
-db.putIfNotExists({
-  _id: type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage,
-  type: type,
-  brand: brand,
-  model: model,
-  color: color,
-  storage: storage,
-  quantity: 0,
-  pricing: {
-    list: 0,
-    retail: 0
-  }
-}).then(function () {
-  return db.get(type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage);
-}).then(function (doc) {
-  $('#alldoc').innerHTML = JSON.stringify(doc.color);
-  console.log(doc);
-}).then(function () {
-  return db.allDocs({include_docs: true});
-}).then(function (result) {
-  console.log(result);
-}).then(function () {
-  return db.allDocs({startkey: 'usb', endkey: 'usb\uffff'});
-}).then(function (result) {
-  console.log(result);
-}).catch(function (err) {
-  console.log(err);
+  db.putIfNotExists({
+    _id: type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage,
+    type: type,
+    brand: brand,
+    model: model,
+    color: color,
+    storage: storage,
+    quantity: 0,
+    pricing: {
+      list: 0,
+      retail: 0
+    }
+  }).then(function () {
+    return db.get(type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage);
+  }).then(function (doc) {
+    $('#alldoc').innerHTML = JSON.stringify(doc.color);
+    console.log(doc);
+  }).then(function () {
+    return db.allDocs({include_docs: true});
+  }).then(function (result) {
+    console.log(result);
+  }).then(function () {
+    return db.allDocs({startkey: 'mobile', endkey: 'mobile\uffff'});
+  }).then(function (result) {
+    console.log(result);
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+  db.query(function (doc, emit) {
+    emit(doc.storage);
+  }, {key: '32gb'}).then(function (result) {
+    console.log(result);
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+  var name = type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage;
+  // // making design doc for above doc
+  var ddoc = {
+    _id: '_design/' + name,
+    views: {
+      by_type: {
+        map: function (doc) {
+          emit(doc.storage);
+        }.toString()
+      }
+    }
+  };
+  db.put(ddoc).then(function () {
+    return db.query(name + '/by_type', {stale: 'update_after'});
+  }).then(function () {
+    return db.query(name + '/by_type', {key: '16gb'});
+  }).then(function (result) {
+    console.log(result);
+  }).catch(function (err) {
+    console.log(err);
+  });
+
 });
 
-var name = type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage;
 
-db.viewCleanup();
 
-PouchDB.replicate('experiment4-db', 'http://localhost:5984/experiment4-db', {live: true});
+// db.viewCleanup();
+
+PouchDB.replicate('experiment5-db', 'http://localhost:5984/experiment5-db', {live: true});
 
 var button = document.createElement('button')
 button.textContent = 'Open window'
