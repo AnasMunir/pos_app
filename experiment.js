4ce8a1cd7e47a8eedf374671bf0e85b915ea6f7f
AnasMunir <anas.munir.92@gmail.com>
2016-10-01 10:09:18 +0500

    refactorde my refacoted code

--- experiment.js
+++ experiment.js
@@ -15,40 +15,6 @@ var storage = '16gb';
 
 productInsertion(db);
 
-// var name = type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage;
-// // // making design doc for above doc
-// var ddoc = {
-//   _id: '_design/' + name,
-//   views: {
-//     by_storage: {
-//       map: function (doc) {
-//         emit(doc.storage);
-//       }.toString()
-//     },
-//     by_type: {
-//       map: function (doc) {
-//         emit(doc.type)
-//       }.toString()
-//     },
-//     reduce: '_sum'
-//   }
-// };
-// db.putIfNotExists(ddoc).then(function () {
-//   return db.query(name + '/by_storage', {stale: 'update_after'});
-//   return db.query(name + '/by_type', {stale: 'update_after'});
-// }).then(function () {
-//   return db.query(name + '/by_storage', {key: '16gb', reduce: true, group: true});
-// }).then(function (result) {
-//   console.log(result);
-// }).then(function () {
-//   return db.query(name + '/by_type', {key: 'usb'});
-// }).then(function (result) {
-//   console.log(result);
-// }).catch(function (err) {
-//   console.log(err);
-// });
-//
-// db.viewCleanup();
 
 function productInsertion(db) {
   db.putIfNotExists({
