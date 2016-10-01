3e9b54a291a704cb0134b00fb39b736d4f88a29e
AnasMunir <anas.munir.92@gmail.com>
2016-10-01 06:02:36 +0500

    cleaned up code from previous commit

--- experiment.js
+++ experiment.js
@@ -11,78 +11,71 @@ var brand = 'sandisk';
 var model = '3.0';
 var color = '';
 var storage = '16gb';
-//
-// new PouchDB('experiment5-db').destroy().then(function () {
-//   return new PouchDB('experiment5-db');
-// }).then(function (db) {
 
-  db.putIfNotExists({
-    _id: type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage,
-    type: type,
-    brand: brand,
-    model: model,
-    color: color,
-    storage: storage,
-    quantity: 0,
-    pricing: {
-      list: 0,
-      retail: 0
+
+db.putIfNotExists({
+  _id: type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage,
+  type: type,
+  brand: brand,
+  model: model,
+  color: color,
+  storage: storage,
+  quantity: 0,
+  pricing: {
+    list: 0,
+    retail: 0
+  }
+}).then(function () {
+  return db.get(type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage);
+}).then(function (doc) {
+  $('#alldoc').innerHTML = JSON.stringify(doc.color);
+  console.log(doc);
+}).then(function () {
+  return db.allDocs({include_docs: true});
+}).then(function (result) {
+  console.log(result);
+}).then(function () {
+  return db.allDocs({startkey: 'mobile', endkey: 'mobile\uffff'});
+}).then(function (result) {
+  console.log(result);
+}).catch(function (err) {
+  console.log(err);
+});
+
+
+var name = type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage;
+// // making design doc for above doc
+var ddoc = {
+  _id: '_design/' + name,
+  views: {
+    by_storage: {
+      map: function (doc) {
+        emit(doc.storage);
+      }.toString()
+    },
+    by_type: {
+      map: function (doc) {
+        emit(doc.type)
+      }.toString()
     }
-  }).then(function () {
-    return db.get(type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage);
-  }).then(function (doc) {
-    $('#alldoc').innerHTML = JSON.stringify(doc.color);
-    console.log(doc);
-  }).then(function () {
-    return db.allDocs({include_docs: true});
-  }).then(function (result) {
-    console.log(result);
-  }).then(function () {
-    return db.allDocs({startkey: 'mobile', endkey: 'mobile\uffff'});
-  }).then(function (result) {
-    console.log(result);
-  }).catch(function (err) {
-    console.log(err);
-  });
+  }
+};
+db.putIfNotExists(ddoc).then(function () {
+  return db.query(name + '/by_storage', {stale: 'update_after'});
+  return db.query(name + '/by_type', {stale: 'update_after'});
+}).then(function () {
+  return db.query(name + '/by_storage', {key: '16gb'});
+}).then(function (result) {
+  console.log(result);
+}).then(function () {
+  return db.query(name + '/by_type', {key: 'usb'});
+}).then(function (result) {
+  console.log(result);
+}).catch(function (err) {
+  console.log(err);
+});
 
-
-  var name = type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage;
-  // // making design doc for above doc
-  var ddoc = {
-    _id: '_design/' + name,
-    views: {
-      by_storage: {
-        map: function (doc) {
-          emit(doc.storage);
-        }.toString()
-      },
-      by_type: {
-        map: function (doc) {
-          emit(doc.type)
-        }.toString()
-      }
-    }
-  };
-  db.putIfNotExists(ddoc).then(function () {
-    return db.query(name + '/by_storage', {stale: 'update_after'});
-    return db.query(name + '/by_type', {stale: 'update_after'});
-  }).then(function () {
-    return db.query(name + '/by_storage', {key: '16gb'});
-  }).then(function (result) {
-    console.log(result);
-  }).then(function () {
-    return db.query(name + '/by_type', {key: 'usb'});
-  }).then(function (result) {
-    console.log(result);
-  }).catch(function (err) {
-    console.log(err);
-  });
-
-// });
-
-
-
-// db.viewCleanup();
+db.viewCleanup();
 
 PouchDB.replicate('experiment5-db', 'http://localhost:5984/experiment5-db', {live: true});
 
@@ -92,11 +85,3 @@ button.addEventListener('click', () => {
 	main.openWindow()
 }, false)
 document.body.appendChild(button)
-
-// db.query(function (doc, emit) {
-//   emit(doc.storage);
-// }, {key: '32gb'}).then(function (result) {
-//   console.log(result);
-// }).catch(function (err) {
-//   console.log(err);
-// });
