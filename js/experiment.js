99f75d144503a9121473c4892d2ff567c541bf89
AnasMunir <anas.munir.92@gmail.com>
2016-10-03 16:32:19 +0500

    Product entery working through form with jquery onclick

--- js/experiment.js
+++ js/experiment.js
@@ -1,58 +1,29 @@
 const remote = require('electron').remote
 const main = remote.require('./main.js')
 
-var $ = document.querySelector.bind(document);
+// var $ = document.querySelector.bind(document);
 
-// var db = new PouchDB('experiment-db');
-//db.plugin(require('pouchdb-upsert');
+var db = new PouchDB('experiment6-db');
+// var PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-upsert'));
 
-var type = 'usb';
-var brand = 'sandisk';
-var model = '2.0';
-var color = '';
-var storage = '16gb';
+$('#getval').click(function () {
+  var barcode = $('#barcode').val();
+  var type = $('#type').val();
+  var brand = $('#brand').val();
+  var model = $('#model').val();
+  var color = $('#color').val();
+  var storage = $('#storage').val();
+
+  productInsertion(barcode, type, brand, model, color, storage);
+});
 
 
-productInsertion(db);
+function productInsertion(barcode, type, brand, model, color, storage) {
 
-// var name = type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage;
-// // // making design doc for above doc
-// var ddoc = {
-//   _id: '_design/' + name,
-//   views: {
-//     by_storage: {
-//       map: function (doc) {
-//         emit(doc.storage);
-//       }.toString()
-//     },
-//     by_type: {
-//       map: function (doc) {
-//         emit(doc.type)
-//       }.toString()
-//     },
-//     reduce: '_sum'
-//   }
-// };
-// db.putIfNotExists(ddoc).then(function () {
-//   return db.query(name + '/by_storage', {stale: 'update_after'});
-//   return db.query(name + '/by_type', {stale: 'update_after'});
-// }).then(function () {
-//   return db.query(name + '/by_storage', {key: '16gb', reduce: true, group: true});
-// }).then(function (result) {
-//   console.log(result);
-// }).then(function () {
-//   return db.query(name + '/by_type', {key: 'usb'});
-// }).then(function (result) {
-//   console.log(result);
-// }).catch(function (err) {
-//   console.log(err);
-// });
-//
-// db.viewCleanup();
-
-function productInsertion(db) {
   db.putIfNotExists({
-    _id: type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage,
+    _id: barcode +'_'+ type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage,
+    barcode: barcode,
     type: type,
     brand: brand,
     model: model,
@@ -64,16 +35,16 @@ function productInsertion(db) {
       retail: 0
     }
   }).then(function () {
-    return db.get(type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage);
+    return db.get(barcode +'_'+ type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage);
   }).then(function (doc) {
-    $('#alldoc').innerHTML = JSON.stringify(doc.color);
+    // $('#alldoc').innerHTML = JSON.stringify(doc);
     console.log(doc);
   }).then(function () {
     return db.allDocs({include_docs: true});
   }).then(function (result) {
     console.log(result);
   }).then(function () {
-    return db.allDocs({startkey: 'mobile', endkey: 'mobile\uffff'});
+    return db.allDocs({startkey: 'usb', endkey: 'usb\uffff'});
   }).then(function (result) {
     console.log(result);
   }).catch(function (err) {
@@ -116,7 +87,7 @@ function productInsertion(db) {
 
 }
 
-PouchDB.replicate('experiment5-db', 'http://localhost:5984/experiment5-db', {live: true});
+PouchDB.replicate('experiment6-db', 'http://localhost:5984/experiment6-db', {live: true});
 
 var button = document.createElement('button')
 button.textContent = 'Open window'
