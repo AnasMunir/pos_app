78bba7f0f37fadaea8269c26d60b6c2beb5bcad5
AnasMunir <anas.munir.92@gmail.com>
2016-10-04 05:21:52 +0500

    product insertion + indexing by name + query all successfull

--- js/experiment.js
+++ js/experiment.js
@@ -1,82 +1,57 @@
 const remote = require('electron').remote
 const main = remote.require('./main.js')
 
-// var $ = document.querySelector.bind(document);
-
 var db = new PouchDB('experiment6-db');
-// var PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-upsert'));
 
 $('#getval').click(function () {
   var barcode = $('#barcode').val();
-  var type = $('#type').val();
-  var brand = $('#brand').val();
-  var model = $('#model').val();
-  var color = $('#color').val();
-  var storage = $('#storage').val();
+  var name = $('#name').val();
 
-  productInsertion(barcode, type, brand, model, color, storage);
+  productInsertion(barcode, name);
 });
 
 
-function productInsertion(barcode, type, brand, model, color, storage) {
+function productInsertion(barcode, name) {
 
   db.putIfNotExists({
-    _id: barcode +'_'+ type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage,
+    _id: barcode +'_'+ name,
     barcode: barcode,
-    type: type,
-    brand: brand,
-    model: model,
-    color: color,
-    storage: storage,
+    product_name: name,
     quantity: 0,
     pricing: {
       list: 0,
       retail: 0
     }
   }).then(function () {
-    return db.get(barcode +'_'+ type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage);
-  }).then(function (doc) {
-    // $('#alldoc').innerHTML = JSON.stringify(doc);
-    console.log(doc);
-  }).then(function () {
     return db.allDocs({include_docs: true});
   }).then(function (result) {
     console.log(result);
   }).then(function () {
-    return db.allDocs({startkey: 'usb', endkey: 'usb\uffff'});
+    return db.allDocs({startkey: '961', endkey: '961\uffff', include_docs: true});
   }).then(function (result) {
     console.log(result);
   }).catch(function (err) {
     console.log(err);
   });
-  var name = type +'_'+ brand +'_'+ model +'_'+ color +'_'+ storage;
+
+  var name = barcode +'_'+ name;
   // // making design doc for above doc
   var ddoc = {
     _id: '_design/' + name,
     views: {
-      by_storage: {
+      by_name: {
         map: function (doc) {
-          emit(doc.storage);
-        }.toString()
-      },
-      by_type: {
-        map: function (doc) {
-          emit(doc.type)
+          emit(doc.product_name);
         }.toString()
       },
       reduce: '_sum'
     }
   };
   db.putIfNotExists(ddoc).then(function () {
-    return db.query(name + '/by_storage', {stale: 'update_after'});
-    return db.query(name + '/by_type', {stale: 'update_after'});
+    return db.query(name + '/by_name', {stale: 'update_after'});
   }).then(function () {
-    return db.query(name + '/by_storage', {key: '16gb', reduce: true, group: true});
-  }).then(function (result) {
-    console.log(result);
-  }).then(function () {
-    return db.query(name + '/by_type', {key: 'usb'});
+    return db.query(name + '/by_name', {startkey: 'liption', endkey: 'liption\uffff', include_docs: true});
   }).then(function (result) {
     console.log(result);
   }).catch(function (err) {
